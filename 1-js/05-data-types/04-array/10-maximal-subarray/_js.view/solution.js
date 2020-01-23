dc7281a93e3999ab8795d87194267de0fbe2c85f
Natascha <46377087+NamikoYa@users.noreply.github.com>
2020-01-23 08:57:53 +0100

    Update solution.js

--- 1-js/05-data-types/04-array/10-maximal-subarray/_js.view/solution.js
+++ 1-js/05-data-types/04-array/10-maximal-subarray/_js.view/solution.js
@@ -1,11 +1,11 @@
-function getMaxSubSum(arr) {
-  let maxSum = 0;
-  let partialSum = 0;
+function getMaxSubSumme(arr) {
+  let maxSumme = 0;
+  let teilSumme = 0;
 
   for (let item of arr) {
-    partialSum += item;
-    maxSum = Math.max(maxSum, partialSum);
-    if (partialSum < 0) partialSum = 0;
+    teilSumme += item;
+    maxSumme = Math.max(maxSumme, teilSumme);
+    if (teilSumme < 0) teilSumme = 0;
   }
-  return maxSum;
+  return maxSumme;
 }
